e3f71dc82576c911cf7736fc04136924bf1559b0
Igor Kushnir <51193240+EmilioRosso@users.noreply.github.com>
2021-03-28 21:02:05 +0300

    full structure

--- components/Contacts/Contacts.jsx
+++ components/Contacts/Contacts.jsx
@@ -1,22 +1,26 @@
 import {Link} from 'next/Link';
 
 import styles from './Contacts.module.scss';
-import contacts from '../../pages/api/contacts'
+import contacts from '../../pages/api/contacts';
 
 export default function Contacts() {
-    return (
-        <section>
-        <div className="container">
-        <h2 className="section__header">Наши контакты</h2>
+  return (
+    <section>
+      <div className='container'>
+        <h2 className='section__header'>Наши контакты</h2>
         <ul className={styles.list}>
-            {contacts.map((contact, i) => <li key={`contact_${i}`} className={styles.item}>
-                <p className={styles.name}>{contact.name}</p>
-                <p className={styles.phone}>{contact.phone}</p>
-                <p className={styles.email}>{contact.email}</p>
-            </li>)}
+          {contacts.map((contact, i) => (
+            <li key={`contact_${i}`} className={styles.item}>
+              <p className={styles.name}>{contact.name}</p>
+              <p className={styles.phone}>{contact.phone}</p>
+              <p className={styles.email}>{contact.email}</p>
+            </li>
+          ))}
         </ul>
-        <Link to="/privacy-policy" className={styles.link}>Политика конфиденциальности</Link>
-        </div>
-        </section>
-    )
+        <Link to='/privacy-policy' className={styles.link}>
+          Политика конфиденциальности
+        </Link>
+      </div>
+    </section>
+  );
 }
