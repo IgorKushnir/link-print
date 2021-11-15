eae690ceb8e698dd0f39a333031329f1d5a92c73
Igor <emilio.rosso.ph@gmail.com>
2021-11-15 12:07:51 +0200

    hotfix

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -42,6 +42,9 @@ export default function Header({ setIsMobileMenuOpened }) {
           </li>
         </ul>
       </div>
+      <div className={styles.telWrapper}>
+        <a href="tel:+380 99 188 16 89">+380 99 188 16 89</a>
+      </div>
       <div className={styles.burgerWrapper}>
         <button
           className={styles.burgerButton}
