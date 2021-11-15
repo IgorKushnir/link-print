ff5d47bcd4f5764b6b06910b5f26f2a206863f4c
Igor <emilio.rosso.ph@gmail.com>
2021-11-15 12:34:11 +0200

    style fixes

--- components/Contacts/Contacts.jsx
+++ components/Contacts/Contacts.jsx
@@ -14,32 +14,52 @@ export default function Contacts() {
             <li key={`contact_${i}`} className={styles.item}>
               <table className={styles.table}>
                 <tbody>
-                <tr>
-                    <td><span className={styles.span}>Адрес:</span></td>
-                    <td><p className={styles.text}>{contact.address}</p></td>
+                  <tr className={styles.tableRow}>
+                    <td className={styles.leftColumn}>
+                      <span className={styles.span}>Адрес:</span>
+                    </td>
+                    <td>
+                      <p className={styles.text}>{contact.address}</p>
+                    </td>
                   </tr>
-                  <tr>
-                    <td><span className={styles.span}>Имя:</span></td>
-                    <td><p className={styles.text}>{contact.name}</p></td>
+                  <tr className={styles.tableRow}>
+                    <td className={styles.leftColumn}>
+                      <span className={styles.span}>Имя:</span>
+                    </td>
+                    <td>
+                      <p className={styles.text}>{contact.name}</p>
+                    </td>
                   </tr>
-                  <tr>
-                    <td><span className={styles.span}>Телефон:</span></td>
-                    <td><a href={`tel:${contact.phone1}`} className={styles.text}>{contact.phone1}</a><a href={`tel:${contact.phone2}`} className={styles.text}>{contact.phone2}</a></td>
+                  <tr className={styles.tableRow}>
+                    <td className={styles.leftColumn}>
+                      <span className={styles.span}>Телефон:</span>
+                    </td>
+                    <td>
+                      <a href={`tel:${contact.phone1}`} className={styles.text}>
+                        {contact.phone1}
+                      </a>
+                      <a href={`tel:${contact.phone2}`} className={styles.text}>
+                        {contact.phone2}
+                      </a>
+                    </td>
                   </tr>
-                  <tr>
-                    <td><span className={styles.span}>E-mail:</span></td>
-                    <td><p className={styles.text}><a href={`mailto:${contact.email}`}>{contact.email}</a></p></td>
+                  <tr className={styles.tableRow}>
+                    <td className={styles.leftColumn}>
+                      <span className={styles.span}>E-mail:</span>
+                    </td>
+                    <td>
+                      <p className={styles.text}>
+                        <a href={`mailto:${contact.email}`}>{contact.email}</a>
+                      </p>
+                    </td>
                   </tr>
                 </tbody>
               </table>
-              
-              
-              
             </li>
           ))}
         </ul>
         <div className={styles.linkWrapper}>
-          <Link href='/privacy-policy.html'>
+          <Link href="/privacy-policy.html">
             <p className={styles.link}>Политика конфиденциальности</p>
           </Link>
         </div>
